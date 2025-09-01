bbf6698ccfe1ffc40018d975b60aeaad1c5b4bea
重铬酸钠 <sthenight@outlook.com>
2025-09-01 15:17:28 +0800

    style: 优化导航卡片样式

--- src/component/NavSection.vue
+++ src/component/NavSection.vue
@@ -11,18 +11,25 @@ defineProps<{
     <div class="nav-section-box">
         <p class="section-name">{{ name }}</p>
         <div class="nav-items-box">
-            <NavItem v-for="item in navItems" :key="item.name" :name="item.name" :link="item.link" />
+            <NavItem v-for="item in navItems" :key="item.name" :name="item.name" :link="item.link"
+                :icon-link="item.iconLink" :description="item.description" />
         </div>
     </div>
 </template>
 
 <style scoped lang="scss">
 @use "@/styles/rule" as *;
+
 .nav-items-box {
     display: grid;
-    grid-template-columns: repeat(2, 1fr);
     gap: 20px;
+    grid-template-columns: repeat(2, 1fr);
+
+    @media screen and #{$mobile-rule} {
+        grid-template-columns: 1fr !important;
+    }
 }
+
 .section-name {
     font-size: 1.2rem;
     font-weight: bold;
